8fc4e2d1722edface8d953af1ba294394ffb50c2
EC2 Default User <ec2-user@ip-172-31-38-241.ec2.internal>
2022-12-02 04:11:37 +0000

    Added script folder

---
+++ scripts/folders.sh
@@ -0,0 +1,5 @@
+mkdir /var/www/dv1
+mkdir /var/www/dv2
+mkdir /var/www/dv3
+mkdir /var/www/dv4
+mkdir /var/www/dv5
---
+++ scripts/s3.sh
@@ -0,0 +1,2 @@
+aws s3 mb  s3://datavail-23767
+aws s3 mb  s3://datavail-23767-logs
